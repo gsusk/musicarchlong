54f8d65156872ef2ccc7fed69bbe0dcb1bb17f70
gsus <satehortuamercado@gmail.com>
2023-10-29 16:16:03 -0500

    added some routes

--- server.js
+++ server.js
@@ -68,16 +68,85 @@ const server = http.createServer((req, res) => {
     /* ========================== ROUTE HANDLERS ========================== */
 
     // Your code here
-    if (req.method === "GET" && req.url.startsWith('/artists/')) {
-      let url = req.url.split('/')
-      if (url.length === 3) {
+    let urlParts = req.url.split('/')
 
+    // Get all the artists
+    if (req.method === "GET" && req.url === "/artists") {
+      res.body = JSON.stringify(artists)
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      return res.end(res.body)
+    }
+
+    // Get a specific artist's details based on artistId
+    if (req.method === "GET" && req.url.startsWith('/artists/')) {
+      if (urlParts.length === 3) {
+        const artistId = urlParts[2];
+        const artist = artists[artistId]
+        if (artist) {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', "application/json");
+          res.body = JSON.stringify(artist);
+          return res.end(res.body);
+        }
       }
 
       res.statusCode = 404;
       res.setHeader("Content-Type", "application/json")
-      return res.end(JSON.stringify({ message: "hola" }))
+      res.write("Endpoint not found");
+      return res.end();
     }
+
+    //add an artist
+    if (req.method === "POST" && req.url === "/artists") {
+      const { name } = req.body;
+      let newArtistId = getNewArtistId();
+      artists[newArtistId] = {
+        artistId: newArtistId,
+        name: name
+      }
+      res.statusCode = 201;
+      res.setHeader("Content-Type", "application/json")
+      res.body = JSON.stringify(artists[newArtistId]);
+      return res.end(res.body)
+    }
+
+    //edit a specific artist by artistId
+    if ((req.method === "PATCH" || req.method === "PUT") && req.url.startsWith("/artist/")) {
+      if (urlParts.length === 3) {
+        const artistId = urlParts[2];
+        const artist = artists[artistId];
+        if (artist) {
+          const { name } = req.body;
+          artist.name = name || artist.name;
+          console.log(artists)
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json")
+          res.body = JSON.stringify({ ...artist, lastUpdated: new Date().toDateString() });
+          return res.end(res.body);
+        }
+      }
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "application/json")
+      res.write("Endpoint: not valid id");
+      return res.end();
+    }
+
+    // Delete a specified artist by artistId
+    if (req.method === "DELETE" && req.url.startsWith("/artists/")) {
+      if (urlParts.length === 3) {
+        const id = urlParts[2];
+        const toBeDeleted = artists[id];
+        if (toBeDeleted) {
+          delete artists[id]
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json")
+          res.body = JSON.stringify({ message: "succesfull deletion" })
+          return res.end(res.body)
+        }
+      }
+    }
+
     res.statusCode = 404;
     res.setHeader('Content-Type', 'application/json');
     res.write("Endpoint not found");
